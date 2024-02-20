3d2a887a8572d8d0e3195b31a3a61ddecaeac7c0
Meww-Meww <meww.meww.02@gmail.com>
2024-02-19 21:06:20 -0500

    Drinking tea gives effects of ingredients

--- src/main/java/by/tealishteam/tealish/items/Tea.java
+++ src/main/java/by/tealishteam/tealish/items/Tea.java
@@ -9,8 +9,11 @@ import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
@@ -31,6 +34,14 @@ public class Tea extends Item {
     @Override
     public ItemStack finishUsingItem(ItemStack itemStack, Level finishUsingItem, LivingEntity entity) {
         super.finishUsingItem(itemStack, finishUsingItem, entity);
+
+        if(itemStack.hasTag()){
+            List<MobEffectInstance> appliedEffects = EffectSerialization.fromCompoundTag(itemStack.getTag());
+            for(MobEffectInstance effect : appliedEffects){
+                entity.addEffect(effect);
+            }
+        }
+
         return new ItemStack(TealishItems.MUG.get());
     }
 
--- src/main/java/by/tealishteam/tealish/items/ingredients/TeaLeaves.java
+++ src/main/java/by/tealishteam/tealish/items/ingredients/TeaLeaves.java
@@ -1,20 +1,12 @@
 package by.tealishteam.tealish.items.ingredients;
 
-import net.minecraft.network.chat.Component;
-import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.TooltipFlag;
-import net.minecraft.world.level.Level;
-
-import javax.annotation.Nullable;
-import java.util.List;
 
 public class TeaLeaves extends Brewable {
     private static final int TEA_LEAVES_COLOR = 0xA2C66A;
     private static final int TEA_LEAVES_COLOR_WEIGHT = 5;
-    private static final MobEffectInstance TEA_LEAVES_EFFECT = new MobEffectInstance(MobEffects.REGENERATION, 10, 1);
+    private static final MobEffectInstance TEA_LEAVES_EFFECT = new MobEffectInstance(MobEffects.REGENERATION, 200, 0);
 
     public TeaLeaves(Properties properties){
         super(properties, TEA_LEAVES_COLOR, TEA_LEAVES_COLOR_WEIGHT, TEA_LEAVES_EFFECT);
